ae7450cdd1429ad459b8ae909aff86b07039c6e0
MrMuhammadSamadSiddiqui <abdulsimplesamad318@gmail.com>
2024-09-26 08:34:21 +0500

    Update LAB 4- Question 1.cpp

--- TASKS/LAB 4/LAB 4- Question 1.cpp
+++ TASKS/LAB 4/LAB 4- Question 1.cpp
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main (){
+int a,n ;
+printf ("Please input a number :\t");
+scanf ("%d" , &n);
+a=n%3;
+if (a==0){
+printf ("%d is a multiple of 3\t " , n);  
+}
+else { 
+printf ("%d is not a multiple of 3 \t" , n); 
+}
+}
